fa7519eefb699534055bd0f2065f3d252219bca7
Billy Hawkes <70971818+billyhawkes@users.noreply.github.com>
2021-12-09 18:42:36 -0500

    Secured api key

--- web/src/service/firebase.ts
+++ web/src/service/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyB6NXOgiTnYGSli1GwdysWOcXJa0_FreJU",
-	authDomain: "revivios.firebaseapp.com",
-	projectId: "revivios",
-	storageBucket: "revivios.appspot.com",
-	messagingSenderId: "510239844080",
-	appId: "1:510239844080:web:cf572e1ed780bec1bc0435",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 	measurementId: "${config.measurementId}",
 };
 

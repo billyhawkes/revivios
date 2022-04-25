43be8a52c0ccafc6c4fa3e8965c318595d251f69
Billy Hawkes <70971818+billyhawkes@users.noreply.github.com>
2022-04-25 12:06:15 -0400

    Removed email password login

--- apps/app/src/pages/auth.tsx
+++ apps/app/src/pages/auth.tsx
@@ -1,15 +1,10 @@
 import { onAuthStateChanged } from "firebase/auth";
-import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import AuthForm from "../components/auth/AuthForm";
 import Providers from "../components/auth/Providers";
-import Tabs from "../components/auth/Tabs";
 import { auth } from "../services/firebase";
 
 const Auth = () => {
 	const router = useRouter();
-	const [authType, setAuthType] = useState<"login" | "register">("login");
 
 	// Handle redirect on authentication
 	onAuthStateChanged(auth, (user) => {
@@ -17,15 +12,8 @@ const Auth = () => {
 	});
 
 	return (
-		<div className="m-auto p-8 mt-[15vh] bg-lightbackground w-96 rounded-lg shadow-md">
-			<Tabs authType={authType} setAuthType={setAuthType} />
+		<div className="m-auto mt-32 p-8 bg-lightbackground w-96 rounded-lg shadow-md">
 			<Providers />
-			<div className="my-8 flex justify-between content-center">
-				<hr className="w-20 h-px bg-white my-auto" />
-				Or continue with
-				<hr className="w-20 h-px bg-white my-auto" />
-			</div>
-			<AuthForm type={authType} />
 		</div>
 	);
 };

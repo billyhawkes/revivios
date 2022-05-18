77563c2868dbb1ccfa188e8839b642b96912c868
Billy Hawkes <70971818+billyhawkes@users.noreply.github.com>
2022-05-18 14:06:29 -0400

    Removed threejs due to import error I couldn't fix

--- apps/website/src/components/landing/Hero/index.tsx
+++ apps/website/src/components/landing/Hero/index.tsx
@@ -1,11 +1,9 @@
 import Intro from "./Intro";
-import Scene3D from "./Scene3D";
 
 const Hero = () => {
 	return (
 		<section className="flex">
 			<Intro />
-			{/* <Scene3D /> */}
 		</section>
 	);
 };
---
+++ apps/website/src/components/landing/Roadmap/index.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const Roadmap = ({}: Props) => {
+	return <div>Roadmap</div>;
+};
+
+export default Roadmap;

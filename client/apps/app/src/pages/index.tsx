bff061405669e41a048b096af0eae333a039460c
Billy Hawkes <70971818+billyhawkes@users.noreply.github.com>
2022-07-28 01:11:04 -0400

    Started total rebuild using t3 stack, setup stack, built basic task crud with database connection

--- client/apps/app/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import type { NextPage } from "next";
-const Home: NextPage = () => {
-	return <>Home</>;
-};
-
-export default Home;
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,39 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+	// Include user.id on session
+	callbacks: {
+		session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id;
+			}
+			return session;
+		},
+	},
+	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		DiscordProvider({
+			clientId: env.DISCORD_CLIENT_ID,
+			clientSecret: env.DISCORD_CLIENT_SECRET,
+		}),
+		GithubProvider({
+			clientId: env.GITHUB_CLIENT_ID,
+			clientSecret: env.GITHUB_CLIENT_SECRET,
+		}),
+		GoogleProvider({
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+		}),
+	],
+};
+
+export default NextAuth(authOptions);

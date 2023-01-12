1f1c9254042849d723b23f3117868572df78b371
billyhawkes <billyhawkes02@gmail.com>
2023-01-12 15:11:23 -0500

    Added hero section code and responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,6 +32,30 @@ const Home: NextPage = () => {
             </Link>
           </nav>
         </header>
+        <section className="mt-20 flex w-full max-w-screen-lg flex-col px-4 sm:mt-40 sm:flex-row sm:px-8">
+          <div className="flex w-full flex-col items-start sm:mr-10 sm:w-[55%]">
+            <h2 className="w-full text-center text-5xl font-bold sm:text-left sm:text-6xl md:text-7xl">
+              Treat life
+            </h2>
+            <h2 className="mt-4 w-full text-center text-5xl font-bold sm:mt-6 sm:text-left sm:text-5xl md:text-6xl">
+              like a <span className="text-primary">game</span>
+            </h2>
+            <h4 className="mt-6 text-center opacity-80 sm:mt-8 sm:text-left">
+              Life is the best game out there. So why not level up yourself in
+              Revivios by managing your time and completing everyday tasks just
+              like a video game.
+            </h4>
+            <Link
+              href="/"
+              className="md:btn invisible h-0 w-0 md:visible md:mt-10 md:h-auto md:w-auto"
+            >
+              Get Started
+            </Link>
+          </div>
+          <div className="mt-8 flex h-32 items-center justify-center rounded-xl bg-lightbackground sm:mt-0 sm:h-auto sm:w-[45%]">
+            <p>Image of character leveling up</p>
+          </div>
+        </section>
       </main>
     </>
   );

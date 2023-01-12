939eb79bf12e233422b6c8dbfcc3d54b9a6b0fab
billyhawkes <billyhawkes02@gmail.com>
2023-01-11 19:31:58 -0500

    Added header navigation w/logo, button design and github link. close #11

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
 import { type NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 const Home: NextPage = () => {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-background to-lightbackground"></main>
+      <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-background to-lightbackground text-white">
+        <header className="flex h-20 w-full max-w-screen-xl items-center justify-between px-4 sm:px-8">
+          <Link href="/" className="flex items-center justify-center ">
+            <Image
+              src="/logo.svg"
+              alt="Logo of Revivios. A teal plus symbol."
+              width={40}
+              height={40}
+            />
+            <h3 className="invisible ml-4 mt-2 w-0 text-2xl font-bold sm:visible sm:w-auto">
+              REVIVIOS
+            </h3>
+          </Link>
+          <nav className="flex items-center justify-center">
+            <Link
+              href="https://github.com/billyhawkes/revivios"
+              target="_blank"
+              className="mt-1 text-lg"
+            >
+              <FaGithub size={40} className="transition hover:opacity-80" />
+            </Link>
+            <Link href="/" className="btn ml-4">
+              Get Started
+            </Link>
+          </nav>
+        </header>
+      </main>
     </>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#07A09B",
-      background: "#1F2727",
-      lightbackground: "#2A3636",
-      error: "#903535",
+    extend: {
+      colors: {
+        primary: "#07A09B",
+        background: "#1F2727",
+        lightbackground: "#2A3636",
+        error: "#903535",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
